ecb196cf89da78794721ef5f5d9084e76cb59d87
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-10 17:03:00 -0400

    Update DevHub.py

--- DevHub.py
+++ DevHub.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import asyncio
 import time
 
-bot = commands.Bot(command_prefix='dh')
+bot = commands.Bot(command_prefix='>')
 TOKEN = os.environ['TOKEN']
 
 @bot.event
