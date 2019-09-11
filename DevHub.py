9792de1355d8b89069a880324fa733a4b7efbea2
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-11 11:18:41 -0400

    Update DevHub.py

--- DevHub.py
+++ DevHub.py
@@ -41,82 +41,18 @@ async def rank(ctx):
     embed.add_field(name="XP", value=f"{user['xp']}")
     await ctx.send(embed=embed)
 
-@bot.command(pass_context=True, aliases=['j', 'joi'])
-async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-
-    await voice.disconnect()
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-        print(f"The bot has connected to {channel}\n")
-
-    await ctx.send(f"Joined {channel])
-
-@bot.command(pass_context=True, aliases=['p', 'pla'])
-async def play(ctx, url: str):
-
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-            print("Removed old song file")
-    except PermissionError:
-        print("Trying to delete song file, but it's being played")
-        await ctx.send("ERROR: Music playing")
-        return
-
-    await ctx.send("Getting everything ready now")
-
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        print("Downloading audio now\n")
-        ydl.download([url])
-
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            name = file
-            print(f"Renamed File: {file}\n")
-            os.rename(file, "song.mp3")
-
-    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.07
-
-    nname = name.rsplit("-", 2)
-    await ctx.send(f"Playing: {nname[0]}")
-    print("playing\n")
-
-    @bot.command(pass_context=True, brief="DO THIS BEFORE ANYTHING ELSE! (ONLY ONCE)", aliases=['reg'])
-    async def register(ctx):
-        id = str(ctx.message.author.id)
-        if id not in amounts:
-            amounts[id] = START_BALANCE
-            await ctx.send(ctx.message.author.mention + ", You are now registered")
-            print(id + " just made an account")
-            print("have to take a dump")
-            _save()
-        else:
-            await ctx.send(ctx.message.author.mention + ", You already have an account")
-            print(id + " just tried to make an account, but already had one")
+@bot.command(pass_context=True, brief="DO THIS BEFORE ANYTHING ELSE! (ONLY ONCE)", aliases=['reg'])
+async def register(ctx):
+    id = str(ctx.message.author.id)
+    if id not in amounts:
+    amounts[id] = START_BALANCE
+    await ctx.send(ctx.message.author.mention + ", You are now registered")
+    print(id + " just made an account")
+    print("have to take a dump")
+    _save()
+   else:
+   await ctx.send(ctx.message.author.mention + ", You already have an account")
+   print(id + " just tried to make an account, but already had one")
 
 
 bot.run(TOKEN)
