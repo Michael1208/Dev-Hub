26fbb4cd4544d6510b72a9bbf47f1e2632b053c6
KyleBurnip <38794622+KyleBurnip@users.noreply.github.com>
2019-09-15 17:25:00 +0100

    Update DevHub.py

--- DevHub.py
+++ DevHub.py
@@ -16,7 +16,7 @@ async def on_ready():
 @bot.check
 def boost(ctx):
     if ctx.message.author.id not in (349499497774055429, 505366642230951984, 578978159488270358):
-        return await ctx.message.channel.send("Premium Required Type n!info For Details")
+        return await ctx.channel.send("Premium Required Type n!info For Details")
     return True
  
 @bot.command()
