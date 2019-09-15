4f2d42f983cfa3a8fa15d1f3e9bc19e89a92c15e
KyleBurnip <38794622+KyleBurnip@users.noreply.github.com>
2019-09-15 17:58:20 +0100

    Devhub.py

--- DevHub.py
+++ DevHub.py
@@ -19,12 +19,18 @@ def owner(ctx):
 
      
 
+class CantDoThatCommand(discord.BaseException):
+    pass
+
 @bot.check
-async def boost(ctx):
+def boost(ctx):
     if ctx.message.author.id not in (349499497774055429, 505366642230951984, 578978159488270358):
-        return await ctx.channel.send("Premium Required Type n!info For Details")
-        return True
-        return False
+        raise CantDoThatCommand("Premium Required Type n!info")
+    return True
+@bot.event
+async def on_error(ctx, error):
+    if isnstance(error, CantDoThatCommand):
+        await ctx.send("Premium Required Type n!info")
  
 @bot.command()
 @commands.check(boost)
